0c4f25fb477a95c95d067d372cb9b7e5963b9d95
Bhaskar P <baskar.baskar91@gmail.com>
2026-01-30 09:45:01 +0530

    generated good morning video

--- app/generate.py
+++ app/generate.py
@@ -10,7 +10,7 @@ API_KEY = os.getenv("GOOGLE_API_KEY", default="")
 
 client = genai.Client(api_key=API_KEY)
 
-prompt = """Aliens are now friends with humans. sharing the knowledge of their technology with each other. if they fight with skills, not sure who wins. can you predict?"""
+prompt = """Namaste! It's a lazy Friday morning in Hyderabad, and nothing beats starting the day with crispy masala dosa and hot filter coffee... wait, make that chai! This dosa is loaded with potatoes, onions, and that perfect spicy chutney kick."""
 
 operation = client.models.generate_videos(
     model="veo-3.1-generate-preview",
@@ -26,5 +26,5 @@ while not operation.done:
 # Download the generated video.
 generated_video = operation.response.generated_videos[0]
 client.files.download(file=generated_video.video)
-generated_video.video.save("dialogue_example.mp4")
+generated_video.video.save("good_morning.mp4")
 print("Generated video saved to dialogue_example.mp4")
